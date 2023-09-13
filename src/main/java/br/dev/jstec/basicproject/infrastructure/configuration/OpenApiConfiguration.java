c845ecc7002f0d58b3c6aedfbe33f3768c02618c
jorge <jorge@jstec.dev.br>
2023-09-12 23:56:18 -0300

    Configurado estrutura de pastas em arq Hexagonal

--- src/main/java/br/dev/jstec/basicproject/infrastructure/configuration/OpenApiConfiguration.java
+++ src/main/java/br/dev/jstec/basicproject/infrastructure/configuration/OpenApiConfiguration.java
@@ -1,4 +1,4 @@
-package br.dev.jstec.basicproject.configuration;
+package br.dev.jstec.basicproject.infrastructure.configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
